29d303948fbccda1112b54d312f2676ab6f3c3e2
Serg <s.ostafiichuk@gmail.com>
2017-02-21 12:16:05 +0200

    Second revision

--- src/Guess2.java
+++ src/Guess2.java
@@ -7,6 +7,7 @@ class Guess2 {
         System.out.println("I'm thinking of a letter between A and Z.");
         System.out.print("Can you guess it: ");
         ch = (char) System.in.read(); // get a char
+        // do some code
         if(ch == answer) System.out.println("** Right **");
         else System.out.println("...Sorry, you're wrong.");
     }
